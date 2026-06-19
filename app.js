8f23288cec4683d84b78a5557499f8c6966d0a89
ajay-nimap <ajay.more@nimapinfotech.com>
2026-06-19 12:47:26 +0530

    Revert "change in port and added .ignorefile"
    This reverts commit 9359335276e0830e626d62711be7380ec6b6e128.

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.get("/users", (req, res) => {
     });
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
